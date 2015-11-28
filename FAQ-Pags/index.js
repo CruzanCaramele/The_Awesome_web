5a508546dbf313b5df9f962f53bc305d1a8c5293
CruzanCaramele <letters2hamza@gmail.com>
2015-11-28 18:19:39 +0800

    use this keyword on the parent element and next keywor to get the sibling then children to get the children, simplify code

--- FAQ-Pags/index.js
+++ FAQ-Pags/index.js
@@ -1,16 +1,20 @@
 // select the id of the DOM element and perform click function on it
 // upon click, slide down the answer
-$("#q1").click(function() {
-	$("#a1").slideToggle("slow");
-	$("#arrow1-down, #arrow1-up").toggleClass("collapse");
+$(".question").click(function() {
+
+	$(this).next().slideToggle("slow");
+	$(this).children().toggleClass("collapse");
+
+	// $("#a1").slideToggle("slow");
+	// $("#arrow1-down, #arrow1-up").toggleClass("collapse");
 })
 
-$("#q2").click(function() {
-	$("#a2").slideToggle("slow");
-	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
-})
+// $("#q2").click(function() {
+// 	$("#a2").slideToggle("slow");
+// 	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
+// })
 
-$("#q3").click(function() {
-	$("#a3").slideToggle("slow");
-	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
-})
+// $("#q3").click(function() {
+// 	$("#a3").slideToggle("slow");
+// 	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
+// })
