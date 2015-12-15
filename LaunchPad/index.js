69811cf7a6bc132283f749ecfc363583c7ac0637
CruzanCaramele <letters2hamza@gmail.com>
2015-12-15 21:30:40 +0800

    access apartment.description and append it to the dom

--- LaunchPad/index.js
+++ LaunchPad/index.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
     	success:  function(response)  {
     		$.each(response.apartments, function(i, apartment) {
 
-                                      var listing = "<a href='#'' class='list-group-item'><h4 class='list-group-item-heading'></h4> <p class='list-group-item-text'></p></a>"
+                                      var listing = "<a href='#'' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description +  "</h4> <p class='list-group-item-text'></p></a>";
+                                      $(".apartments").append(listing);
 
 
     		});
