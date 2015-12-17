27a2408deb0c29d15ec6f783a0e50be5fcee96ab
CruzanCaramele <letters2hamza@gmail.com>
2015-12-17 21:33:45 +0800

    Filter out cities that DON'T match our selected borough, add listings class, and apartmentClass variable

--- LaunchPad/index.js
+++ LaunchPad/index.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
     	success:  function(response)  {
     		$.each(response.apartments, function(i, apartment) {
 
-                                      var listing = "<a href='#'' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price +   "</h4> <p class='list-group-item-text'>"  + apartment.neighborhood + "</p></a>";
+                                     var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+                                      var listing = "<a href='#'' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price +   "</h4> <p class='list-group-item-text'>"  + apartment.neighborhood + "</p></a>";
                                       $(".apartments").append(listing);
 
 
@@ -20,8 +21,11 @@ $(document).ready(function() {
     });
 
     $(".filter").click(function(){
-        
-    })
+        var city = $(this).attr("id");
+
+        $(".listings").not("." + city).css("display", "none");
+
+    });
 });
 
 
